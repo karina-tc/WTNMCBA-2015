036e183ba86450a4ddd1dfdab88f2ffeff175086
Karina <kl.tovar@gmail.com>
2015-03-11 13:07:33 -0400

    CMT 2 - Added Coming Soon Page

---
+++ soon/header_soon.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css" />
+  		<link rel="stylesheet" href="css/wtnmcba.css" />
+  		
+  		<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
+  		
+
+		
+
+    </head>
+    <body>
+
+    <div id="site-wrap"> <!-- start website wrap -->
+
+
+    	
---
+++ soon/index.php
@@ -0,0 +1,20 @@
+<?php
+  $pageTitle = "West Texas New Mexico Custom Broker Association | Coming Soon";
+  $metaDescription = "";
+  include("header_soon.php");
+?>
+    
+
+
+    <div id="soon-banner" class="pure-u-1">
+        <img src="img/hp-banner-logo.png">
+        <h1 style="color:#fff; font-weight:400; margin-top:-1%; letter-spacing:.1em; font-size:28px;">SERVING THE TRADE COMMUNITY</h1>
+        <h2 style="color:#fff; font-size:11px; text-transform:uppercase; letter-spacing:5px; font-weight:300;">NEW WEBSITE COMING SOON</h2>
+    </div>
+
+  
+
+
+
+      
+<?php include("footer_soon.php"); ?>
